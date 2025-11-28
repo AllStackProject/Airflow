a776fd30b6808ce71f079aa51717d33526b8a1bc
YongHun Lee <56614731+dldydgns@users.noreply.github.com>
2025-11-28 11:47:17 +0900

    Feat: NodeSelector 추가

--- video.py
+++ video.py
@@ -67,6 +67,7 @@ def exec_config(container):
                 ],
                 node_selector={
                     "role": "airflow-worker"
+                    "kubernetes.io/hostname": "ip-10-0-0-12"
                 },
                 containers=[
                     k8s.V1Container(
