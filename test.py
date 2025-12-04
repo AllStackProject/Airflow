2cd25e7e9d115f5ec50fa80e4feaba5912f1f8c1
YongHun Lee <56614731+dldydgns@users.noreply.github.com>
2025-12-04 14:23:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.python import get_current_context
 from airflow.models import Variable
 from datetime import datetime
 import requests
@@ -20,62 +19,28 @@ def test_success_callback(context):
 def test_failure_callback(context):
     print("🔥 FAILURE CALLBACK 실행됨!")
 
+    ti = context["task_instance"]
     dag_conf = context["dag_run"].conf
+
     org_id = dag_conf.get("org_id")
     video_uuid = dag_conf.get("video_uuid")
+    exception = context.get("exception")
 
-    # 실패한 Task 정보
-    text = str(context['task_instance'])
+    text = f"""
+🔥 *Task Failed!*
 
-    # 예외 정보
-    exception = context.get('exception')
-    if exception:
-        text += f"\nException: ```{str(exception)}```"
+*Task Info*
+- Task ID: `{ti.task_id}`
+- DAG ID: `{ti.dag_id}`
+- Run ID: `{ti.run_id}`
+- Try Number: {ti.try_number}
+- Hostname: {ti.hostname}
+- State: {ti.state}
 
-    # 추가 정보
-    text += f"\nvideo_uuid = {video_uuid}"
-    text += f"\norg_id = {org_id}"
+*Config*
+- video_uuid: `{video_uuid}`
+- org_id: `{org_id}`
 
-    # Slack URL
-    slack_url = f"https://hooks.slack.com/services/{Variable.get('slack_url')}"
-
-    payload = {
-        "user_name": "airflow",
-        "text": text,
-        "icon_emoji": ":cry:"
-    }
-
-    headers = {"content-type": "application/json"}
-
-    # Slack 전송
-    requests.post(slack_url, json=payload, headers=headers)
-
-
-# --------------------------
-# Task
-# --------------------------
-@task
-def success_task():
-    print("task 성공 실행")
-
-
-@task
-def fail_task():
-    print("task 실패 실행 예정")
-    raise ValueError("일부러 예외 발생!")
-
-
-# --------------------------
-# DAG
-# --------------------------
-with DAG(
-    dag_id="callback_test_dag",
-    start_date=datetime(2025, 1, 1),
-    schedule=None,
-    catchup=False,
-    default_args={
-        "on_success_callback": test_success_callback,
-        "on_failure_callback": test_failure_callback,
-    }
-) as dag:
-    fail_task()
+*Exception*
+```python
+{exception}
